2bbf2664964b74b8b6a170522d8972260e704773
Eduardo Azevedo <you@example.com>
2023-08-29 22:24:47 -0300

    Camisas VEK

---
+++ src/Screens/Home/style.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+
+    fundo: {
+      backgroundColor: '#0B2447',
+    },
+
+    container: {
+      flex:1,
+      alignItems:"center",
+      paddingBottom: 20,
+    },
+  
+    card: {
+      width:"95%", 
+      borderRadius:'10px', 
+      marginTop:'5%',
+    },
+
+    cardCamisa: {
+      border: '2px #1c1c1c solid'
+    }
+})
+
+export default styles;
